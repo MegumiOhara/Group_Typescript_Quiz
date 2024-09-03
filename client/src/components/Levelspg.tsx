c13eee54a47764675c7517aa119d1457194c9e97
Yui Jensen <kyanyui67@gmail.com>
2024-09-03 12:26:33 +0200

    Added node module file

--- client/src/components/Levelspg.tsx
+++ client/src/components/Levelspg.tsx
@@ -7,7 +7,7 @@ function Levels() {
 
     return (
         <div className="container">
-        <div className="levels">
+        <div className="levels"> 
             <h1>Choose the level</h1>
                 <button><Link to="/quiz"> Level 1</Link></button>
                 <button><Link to="/quizTwo">Level 2</Link></button>
