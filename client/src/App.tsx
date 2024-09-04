d5c7dad84a2e7e97374f3b6f7928ae979fe7c7fd
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-09-04 14:31:43 +0200

    deleted repetitive components
    quiz, questions T, Levelspg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
-import Quiz from './components/Quiz';
 import Welcome from './components/Welcome';
 import RegisterForm from './components/NewReg';
-import Levels from './components/Levelspg';
 import Navbar from './components/Navbar';
 import QuizTwo from './components/Quiztwo';
 import { useState } from 'react';
@@ -24,8 +22,6 @@ function App() {
         <Routes>
             <Route path="/" element={<RegisterForm onRegister={handleRegister} />}/>
             <Route path="/welcome" element={<Welcome player={registeredName} fontSize= {30} backgroundColor= "navy" />}/> 
-            <Route path= "/levelpg" element={<Levels />}/>
-            <Route path= "/quiz" element={<Quiz />}/> 
             <Route path= "/quizTwo" element={<QuizTwo />}/>
           
         </Routes>
