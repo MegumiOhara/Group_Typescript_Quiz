2ffe0875a616bfccda3e4c2f38d37233e6217268
Yui Jensen <kyanyui67@gmail.com>
2024-09-05 11:49:29 +0200

    Remove React
    I had an error of "'React' is declared but its value is never read." Our React is 18 version, higher than the 17 version so removed it

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-//import React from 'react';
+//import React from "react";  'React' is declared but its value is never read. I remove React because our react version is higher than 17
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
