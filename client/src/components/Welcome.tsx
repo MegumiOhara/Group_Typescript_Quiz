99aae14d72483cd5195667b1f0c82c7dc1adc9b1
Mary Dawn Malida <157961578+PseuDawncode@users.noreply.github.com>
2024-09-05 14:14:26 +0200

    button component applied

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React from 'react';
+import Button from '../components/Button'; //need to import the reusable button here to call the styling from the component of Button.tsx
+import { useNavigate } from 'react-router-dom';
 
 
 type WelcomeProps = {
@@ -10,19 +11,25 @@ type WelcomeProps = {
 
 
 const Welcome = ({ player, fontSize, backgroundColor }:WelcomeProps) => {
+   const navigate = useNavigate();
+   const handleButtonClick = () => {
+      navigate("/quizTwo");
+   }
    return (
       
       <div style={{ fontSize, backgroundColor}}>
          <h1 className="animate__animated animate__heartBeat animate__repeat-3	3">Welcome, {player}!</h1>
          <p>This is a Quiz to test  your Swedish skills!</p>
          <p>Choose one correct answer from 4 multiple choice.</p>
-         <button><Link to="/levelpg">Choose the level</Link></button>
+
+         <Button label="Choose the level" onClick={handleButtonClick}/>
       </div>
     
    );
 };
 
 export default Welcome;
+
 
 //on App.tsx add 
 // <Route path="/welcome" element={<Welcome player={registeredName} fontSize= {30} backgroundColor= "navy" />}/>
