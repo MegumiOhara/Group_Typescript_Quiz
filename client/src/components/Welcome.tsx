a3ce6e1e1b6e532710cf514749da0417c5211ffb
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-09-05 12:55:53 +0200

    css tailwind for welcome box

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -4,26 +4,24 @@ import { useNavigate } from 'react-router-dom';
 
 type WelcomeProps = {
    player: string;
-   fontSize: number;
-   backgroundColor: string;
+   
 }
 
 
-const Welcome = ({ player, fontSize, backgroundColor }:WelcomeProps) => {
+const Welcome = ({ player }:WelcomeProps) => {
    const navigate = useNavigate();
    const handleButtonClick =() => {
       navigate("/quizTwo")
    };
    return (
       
-      <div style={{ fontSize, backgroundColor}}>
-         <h1>Welcome, {player}!</h1>
+      <div className="m-11 p-6 h-72 max-w-md mx-auto bg-blue-300 leading-10 rounded-xl shadow-md flex flex-col justify-center items-center" >
+         <p className= "animate-bounce text-white font-semibold text-2xl drop-shadow-xl">Welcome, {player}!</p>
          <p>This is a Quiz to test  your Swedish skills!</p>
          <p>Choose one correct answer from 4 multiple choice.</p>
          <Button label='Start Game' onClick={handleButtonClick}></Button>
-              
       </div>
-    
+     
    );
 };
 
