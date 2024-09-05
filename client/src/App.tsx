bfc262b9a1fc8e1515ab4e2f2d4a699cd22a0fd8
Yui Jensen <kyanyui67@gmail.com>
2024-09-05 10:04:57 +0200

    Add useNavigate to able to connect link
    To use useNavigate from react-router-dom to link welcome component to quiz component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,50 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
-import Welcome from './components/Welcome';
-import RegisterForm from './components/NewReg';
-import Navbar from './components/Navbar';
-import QuizTwo from './components/Quiztwo';
-import { useState } from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+import Welcome from "./components/Welcome";
+import RegisterForm from "./components/NewReg";
+import Navbar from "./components/Navbar";
+import QuizTwo from "./components/Quiztwo";
+import { useState } from "react";
 
 function App() {
-  const [registeredName, setRegisteredName] = useState<string>('');
-  
-  const handleRegister = (player:string) => {
+  const [registeredName, setRegisteredName] = useState<string>("");
+
+  const handleRegister = (player: string) => {
     setRegisteredName(player);
   };
 
   return (
     <BrowserRouter>
-    <div>
-      <Navbar />
-    </div>
-    <div>
-        <Routes>
-            <Route path="/" element={<RegisterForm onRegister={handleRegister} />}/>
-            <Route path="/welcome" element={<Welcome player={registeredName} fontSize= {30} backgroundColor= "navy" />}/> 
-            <Route path= "/quizTwo" element={<QuizTwo />}/>
-          
-        </Routes>
-        
+      <div>
+        <Navbar />
       </div>
-    </BrowserRouter> 
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={<RegisterForm onRegister={handleRegister} />}
+          />
+          <Route
+            path="/welcome"
+            element={
+              <Welcome
+                player={registeredName}
+                fontSize={30}
+                backgroundColor="navy"
+              />
+            }
+          />
+          <Route path="/quizTwo" element={<QuizTwo />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-
+import React from "react";
+//import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 type WelcomeProps = {
-   player: string;
-   fontSize: number;
-   backgroundColor: string;
-}
+  player: string;
+  fontSize: number;
+  backgroundColor: string;
+};
 
-
-const Welcome = ({ player, fontSize, backgroundColor }:WelcomeProps) => {
-   return (
-      
-      <div style={{ fontSize, backgroundColor}}>
-         <h1 className="animate__animated animate__heartBeat animate__repeat-3	3">Welcome, {player}!</h1>
-         <p>This is a Quiz to test  your Swedish skills!</p>
-         <p>Choose one correct answer from 4 multiple choice.</p>
-         <button><Link to="/quizTwo">Choose the level</Link></button>
-      </div>
-    
-   );
+const Welcome = ({ player, fontSize, backgroundColor }: WelcomeProps) => {
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate("/quizTwo");
+  };
+  return (
+    <div style={{ fontSize, backgroundColor }}>
+      <h1 className="animate__animated animate__heartBeat animate__repeat-3	3">
+        Welcome, {player}!
+      </h1>
+      <p>This is a Quiz to test your Swedish skills!</p>
+      <p>Choose one correct answer from 4 multiple choice.</p>
+      <Button label="Choose the level" onClick={handleButtonClick} />
+    </div>
+  );
 };
 
 export default Welcome;
 
-//on App.tsx add 
+//on App.tsx add
 // <Route path="/welcome" element={<Welcome player={registeredName} fontSize= {30} backgroundColor= "navy" />}/>
